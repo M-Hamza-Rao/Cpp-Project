dd091d6629854ccab531aa56fef36d11df633523
M-Hamza-Rao <rmuhammadhamza86@gmail.com>
2025-04-24 23:28:23 +0200

    main file

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "hotel.h"
+
+using namespace std;
+
+int main() {
+    Hotel h;
+
+    // Connect to DB
+    if (!h.connectDB()) {
+        return 1;  // Exit if DB connection fails
+    }
+
+    // Create tables and populate rooms
+    h.setupTables();
+    h.populateRooms();
+
+    // Now you can start implementing your hotel management logic
+    // Example: checking available rooms
+    h.checkAvailableRooms();
+
+    // Exit
+    h.closeDB();
+
+    return 0;
+}
