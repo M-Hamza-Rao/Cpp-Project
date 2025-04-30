f1192c32ab64ad14984d9d787fe4b09b6d5dac77
M-Hamza-Rao <rmuhammadhamza86@gmail.com>
2025-04-30 23:44:42 +0200

    main

--- main.cpp
+++ main.cpp
@@ -6,20 +6,10 @@ using namespace std;
 int main() {
     Hotel h;
 
-    // Connect to DB
     if (!h.connectDB()) {
-        return 1;  // Exit if DB connection fails
-    }
-
-    // Create tables and populate rooms
-    //h.setupTables();
-    //h.populateRooms();
-
-    // Now you can start implementing your hotel management logic
-    // Example: checking available rooms
+        return 1;  
     h.checkAvailableRooms();
 
-    // Exit
     h.closeDB();
 
     return 0;
